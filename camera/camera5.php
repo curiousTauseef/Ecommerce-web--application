803fbc2634a79a98b3153f27b0343fde9bbff024
shahan hasan <40837211+shahanHasan@users.noreply.github.com>
2019-03-04 03:58:36 +0000

    Add files via upload

---
+++ camera/camera5.php
@@ -0,0 +1,29 @@
+<?php 
+require_once '../controller/DB.php' ;
+include 'head.php';
+include 'navigationBar.php';
+
+$sql5 = "SELECT * FROM cameradetails WHERE cameraID = 5";
+$products5 = $db->query($sql5); 
+
+?>
+<h2 style="text-align: center;font-family:Courier New,Courier,monospace;font-style: Bold;color : black ;font-size : 250%;"> Camera Specification</h2>
+
+<?php while($product = mysqli_fetch_assoc($products5)) : ?>
+<img src="../<?=$product['imageLocation'];?>" alt="<?= $product['cameraName']; ?>" class="center" style="width: 20%;height: auto;">
+<div class="hero-image">
+<h3 style="text-align: center;font-family:Courier New,Courier,monospace;font-style: Bold;color : white ;font-size : 250%;background-color: black;padding-left: 20px;padding-right: 20px;font-size: 120%;""><?= $product['cameraName']; ?></h3>
+<h4 class="list-price text-danger" style="text-align: center;font-family:Courier New,Courier,monospace;font-style: Bold;color : white ;font-size : 250%;background-color: black;padding-left: 20px;padding-right: 20px;font-size: 120%;"">List Price :<s><?= $product['cameraPrice']; ?></s></h4>
+<h3 class="price" style="text-align: center;font-family:Courier New,Courier,monospace;font-style: Bold;color : white ;font-size : 250%;  background-color: black;padding-left: 20px;padding-right: 20px;font-size: 120%;">Our Price : <?= $product['cameraActualPrice']; ?></h3>
+<h5 style="text-align: center;font-family:Courier New,Courier,monospace;font-style: Bold;color : white ;font-size : 250%;background-color: black;padding-left: 20px;padding-right: 20px;font-size: 120%;">Camera Description : </h5>
+<h6 style="text-align: center;font-family:Courier New,Courier,monospace;font-style: Bold;color : white ;font-size : 190%;background-color: black;padding-left: 20px;padding-right: 20px;font-size: 120%;"><?= $product['cameraDescription']; ?></h6>
+<h1 style="text-align: center;font-family:Courier New,Courier,monospace;font-style: Bold;color : white ;font-size : 250%;background-color: black;padding-left: 20px;padding-right: 20px;font-size: 120%;">Camera Brand : <?= $product['cameraBrand']; ?></h1>
+<h2 style="text-align: center;font-family:Courier New,Courier,monospace;font-style: Bold;color : white ;font-size : 250%;background-color: black;padding-left: 20px;padding-right: 20px;font-size: 120%;">Camera Model : <?= $product['cameraModel']; ?></h2>
+<h5 style="text-align: center;font-family:Courier New,Courier,monospace;font-style: Bold;color : white ;font-size : 250%;background-color: black;padding-left: 20px;padding-right: 20px;font-size: 120%;">Lens Description : <?= $product['lensDesc']; ?></h5>
+</div>
+<?php endwhile; ?>
+
+
+<?php include '../homefooter.php'?>
+</body>
+</html>
---
+++ camera/navigationBar.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-inverse ">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand">Portrait Simple</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="../homepage.php"><span class="glyphicon glyphicon-home"></span> Home</a></li>
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#"> Products and Services<span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="../Products.php"><span class="glyphicon glyphicon-camera"></span>CAMERA'S</a></li>
+          <li><a href="../Detailed.php"><span class="glyphicon glyphicon-camera">FULL SPECIFICATION</a></li>
+
+        </ul>
+      </li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="../Help&Support.php"><span class="glyphicon glyphicon-thumbs-up"></span> Help</a></li>
+      <li><a href="../CustomerService.php"><span class="glyphicon glyphicon-cloud"></span> Customer Service</a></li>
+	  <li><a href="../Abouts.php"><span class="glyphicon glyphicon-flag"></span> About us</a></li>
+    </ul>
+  </div>
+</nav>
+
+
